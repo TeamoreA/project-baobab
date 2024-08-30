1125347563656ed8f82743d58765a2c95b6acf17
Tim Kamau <tim@cloudkite.io>
2024-08-30 13:26:07 +0300

    removed unused files

--- src/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from src.controllers.domain_lookup_controller import domain_lookup
-
-# main blueprint to be registered with application
-api = Blueprint('api', __name__)
-
-# register user with api blueprint
-api.register_blueprint(domain_lookup)
